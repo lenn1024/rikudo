217dd19b64c693d7a1626377f77edf06e181adfe
guanshiming <lenn1024@163.com>
2019-08-06 11:33:59 +0800

    add netty server demo && add log config

--- rikudo-netty-action/src/main/java/ai/code/practise/rikudo/netty/simple/Server.java
+++ rikudo-netty-action/src/main/java/ai/code/practise/rikudo/netty/simple/Server.java
@@ -9,12 +9,19 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.AttributeKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 使用netty创建的一个简单的server demo
  */
 public class Server {
+
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) throws InterruptedException {
+        logger.info("启动服务端.");
+
         EventLoopGroup masterGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
@@ -33,6 +40,7 @@ public class Server {
             ChannelFuture channelFuture = serverBootstrap.bind(8000).sync();
             channelFuture.channel().closeFuture().sync();
         } finally {
+            logger.info("关闭服务端.");
             masterGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
