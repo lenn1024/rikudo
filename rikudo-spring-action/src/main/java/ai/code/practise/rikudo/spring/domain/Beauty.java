d6d378cc38d7a41139b7c4e976db895fcbd33aa7
guanshiming <lenn1024@163.com>
2019-09-17 14:58:45 +0800

    手动构造一个Ioc容器

---
+++ rikudo-spring-action/src/main/java/ai/code/practise/rikudo/spring/domain/Beauty.java
@@ -0,0 +1,14 @@
+package ai.code.practise.rikudo.spring.domain;
+
+import lombok.Data;
+
+/**
+ * 定义一个美女
+ */
+@Data
+public class Beauty {
+
+    private String name;
+
+    private int age;
+}
---
+++ rikudo-spring-action/src/main/java/ai/code/practise/rikudo/spring/ioc/BeanFactoryMain.java
@@ -0,0 +1,30 @@
+package ai.code.practise.rikudo.spring.ioc;
+
+import ai.code.practise.rikudo.spring.domain.Beauty;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * 手动初始化一个Ioc容器
+ */
+@Slf4j
+public class BeanFactoryMain {
+
+    public static void main(String[] args){
+        // 初始化资源访问
+        Resource resource = new ClassPathResource("spring/spring-ioc.xml");
+        // 构造Ioc容器
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 构造读取BeanDefinition的Reader
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // 加载bean定义
+        reader.loadBeanDefinitions(resource);
+
+        // 使用构造好的Ioc容器
+        Beauty beauty = beanFactory.getBean(Beauty.class);
+        log.info("华哥今天上了一个美女: {}.", beauty);
+    }
+}
